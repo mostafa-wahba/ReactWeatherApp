0a253302e324334d1b72665129e8492dd6bea87b
mostafa wahba <mostafawahba22710@gmail.com>
2023-04-23 19:00:34 +0200

    Updates

---
+++ precache-manifest.35fb5a8aecd5f4584848301bfd92c810.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd7409d000f2796de8be",
+    "url": "/ReactWeatherApp/static/css/main.c9b077e8.chunk.css"
+  },
+  {
+    "revision": "bd7409d000f2796de8be",
+    "url": "/ReactWeatherApp/static/js/main.bd7409d0.chunk.js"
+  },
+  {
+    "revision": "9e6c96c48f2fd00bc38b",
+    "url": "/ReactWeatherApp/static/js/1.9e6c96c4.chunk.js"
+  },
+  {
+    "revision": "09c3b4c557a847f2e8f7",
+    "url": "/ReactWeatherApp/static/css/2.29ee42ce.chunk.css"
+  },
+  {
+    "revision": "09c3b4c557a847f2e8f7",
+    "url": "/ReactWeatherApp/static/js/2.09c3b4c5.chunk.js"
+  },
+  {
+    "revision": "446bc9525bafd27ae76b",
+    "url": "/ReactWeatherApp/static/js/runtime~main.446bc952.js"
+  },
+  {
+    "revision": "a31b0c7b351f345ec2cda7c0c27fea2a",
+    "url": "/ReactWeatherApp/static/media/728093.a31b0c7b.png"
+  },
+  {
+    "revision": "6c28dfd3d0674191f38e209b476c9e96",
+    "url": "/ReactWeatherApp/static/media/83d3937e2c788df111969f1977f2f5c0.6c28dfd3.png"
+  },
+  {
+    "revision": "0037905bb78da51f3778aabd0692ce43",
+    "url": "/ReactWeatherApp/static/media/visibility.0037905b.png"
+  },
+  {
+    "revision": "06888416bc84a05c299e307736d96a3b",
+    "url": "/ReactWeatherApp/static/media/pngwing.com.06888416.png"
+  },
+  {
+    "revision": "614c053bba2fa5475d91a1e46dccf1a5",
+    "url": "/ReactWeatherApp/static/media/toppng.com-rain-cloud-shape-with-a-few-raindrops-icon-cartoon-water-drops-449x738.614c053b.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ReactWeatherApp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ReactWeatherApp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ReactWeatherApp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ReactWeatherApp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ReactWeatherApp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "4f50ce2b4858db32491ace07551fe878",
+    "url": "/ReactWeatherApp/index.html"
+  }
+];
